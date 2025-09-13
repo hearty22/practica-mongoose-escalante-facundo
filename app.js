ff471018e8abbe6b699e7f96ccafac24ee6e8dac
hearty_dev <facudnoesclante@gmail.com>
2025-09-13 13:10:40 -0300

    servidor basico con estructura principal

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-//oo
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT
+app.use(express.json());
+app.use(cors());
+
+
+app.listen(PORT, ()=>{
+    console.log(`servidor escuchando en http://localhost:${PORT}`);
+})
